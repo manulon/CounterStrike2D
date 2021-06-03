98683366f7f8987d6cdefa05797ec8b504335f5f
manulon <mlongoe@fi.uba.ar>
2021-06-03 19:47:34 -0300

    Primer commit

---
+++ MATERIAL/Ejemplo-Clase-SDL/src/CMakeFiles/sdl-alpha-blending.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/manulon/Escritorio/CounterStrike2D/Ejemplo-Clase-SDL/src/mainSdlAlphaBlending.cpp" "/home/manulon/Escritorio/CounterStrike2D/Ejemplo-Clase-SDL/src/CMakeFiles/sdl-alpha-blending.dir/mainSdlAlphaBlending.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../libs"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/manulon/Escritorio/CounterStrike2D/Ejemplo-Clase-SDL/src/libs/CMakeFiles/sdldemo.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ MATERIAL/Ejemplo-Clase-SDL/src/libs/CMakeFiles/sdldemo.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/manulon/Escritorio/CounterStrike2D/Ejemplo-Clase-SDL/libs/Animation.cpp" "/home/manulon/Escritorio/CounterStrike2D/Ejemplo-Clase-SDL/src/libs/CMakeFiles/sdldemo.dir/Animation.cpp.o"
+  "/home/manulon/Escritorio/CounterStrike2D/Ejemplo-Clase-SDL/libs/Area.cpp" "/home/manulon/Escritorio/CounterStrike2D/Ejemplo-Clase-SDL/src/libs/CMakeFiles/sdldemo.dir/Area.cpp.o"
+  "/home/manulon/Escritorio/CounterStrike2D/Ejemplo-Clase-SDL/libs/Exception.cpp" "/home/manulon/Escritorio/CounterStrike2D/Ejemplo-Clase-SDL/src/libs/CMakeFiles/sdldemo.dir/Exception.cpp.o"
+  "/home/manulon/Escritorio/CounterStrike2D/Ejemplo-Clase-SDL/libs/SdlException.cpp" "/home/manulon/Escritorio/CounterStrike2D/Ejemplo-Clase-SDL/src/libs/CMakeFiles/sdldemo.dir/SdlException.cpp.o"
+  "/home/manulon/Escritorio/CounterStrike2D/Ejemplo-Clase-SDL/libs/SdlTexture.cpp" "/home/manulon/Escritorio/CounterStrike2D/Ejemplo-Clase-SDL/src/libs/CMakeFiles/sdldemo.dir/SdlTexture.cpp.o"
+  "/home/manulon/Escritorio/CounterStrike2D/Ejemplo-Clase-SDL/libs/SdlWindow.cpp" "/home/manulon/Escritorio/CounterStrike2D/Ejemplo-Clase-SDL/src/libs/CMakeFiles/sdldemo.dir/SdlWindow.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
