46923b427ad0769bd44d79e174a272eb1adc9410
manulon <mlongoe@fi.uba.ar>
2021-06-03 20:14:30 -0300

    Tengo problemas con el include

---
+++ Semana1/CMakeFiles/showAnImage.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/showAnImage.dir/showAnImage.cpp.o"
+  "showAnImage"
+  "showAnImage.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/showAnImage.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
