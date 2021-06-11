3a746e49e1021485db0a7227a71b76846860ec95
DanieLovera <daanilo2497@gmail.com>
2021-06-11 01:29:01 -0300

    Agrego al main el stencil.
    	- Ya funciona salvo con el bug mencionado en su commit.
    	- Aun no se implementa la deteccion de eventos de mouse
    	- Para poder rotar el renderizado del stencil.

--- test/showAnImage.cpp
+++ test/showAnImage.cpp
@@ -1,6 +1,7 @@
 #include "Window.h"
 #include "Image.h"
 #include "Area.h"
+#include "Stencil.h"
 #include <SDL2/SDL.h>
 #include <exception>
 #include <iostream>
@@ -8,14 +9,17 @@
 
 int main(int argc, const char *argv[]){
     try {
-        Window window("Ventana de prueba", 800, 600, SDL_WINDOW_RESIZABLE);
+        Window window("Counter Strike 2D", 800, 600, SDL_WINDOW_RESIZABLE);
         Image image("assets/gfx/backgrounds/toxic.jpg", window);
-        Area destArea(325, 225, 150, 150);
-        image.render(destArea);
+        Stencil stencil(1000, 1000, 25, 90, 150, window); 
+        Area imageArea(0, 0, 800, 600);
+        Area stencilArea((800/2)-(1000/2), (600/2)-(1000/2), 1000, 1000);
+        image.render(imageArea);
+        stencil.render(stencilArea, 0);
         window.render();
         usleep(3000000);
 
-    } catch (std::exception& e) {
+    } catch (std::exception &e) {
         std::cout << e.what() << std::endl;
         return 1;
     }
