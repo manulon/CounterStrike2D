bca3fab85064bcc3e44edd44590b780a6c605df0
DanieLovera <daanilo2497@gmail.com>
2021-06-11 01:27:31 -0300

    Cargo la clase stencil configurable.
    	- Hay un bug si el angulo, que se intenta usar es mayor a
    	  180 grados.
    	- TODO corregir.w

---
+++ include/Stencil.h
@@ -0,0 +1,35 @@
+#ifndef _STENCIL_H_
+#define _STENCIL_H_
+
+#include "SdlTexture.h"
+class SdlRenderer;
+class Window;
+class Area;
+
+class Stencil {
+    private:
+        SdlTexture sdlTexture;
+        SdlRenderer &sdlRenderer;
+        int width;
+        int height;
+
+        Stencil(const Stencil &other) = delete;
+        Stencil& operator=(const Stencil &other) = delete;
+        Stencil& operator=(Stencil &&other) = delete;
+        void fillBackground();
+        void drawStencil(int Xc, int Yc, int viewR, float viewAngle);
+        void drawCircle(int Xc, int Yc, int r);
+        
+        // Trabaja con maximo 180 grados de vision 
+        // TODO Aumentar la capacidad a mas de 180 grados.
+        void drawViewAngle(int viewAngle);
+        void convertTexture();
+    
+    public:
+        Stencil(int width, int height, int viewR, int viewAngle, uint8_t alpha, Window &window);
+        Stencil(Stencil &&other);
+        ~Stencil();
+        void render(const Area &dest, const double angle) const;
+};
+
+#endif // _STENCIL_H_
