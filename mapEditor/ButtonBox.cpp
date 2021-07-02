e17c22ee039bd9b39ffc02dd804f0038b982951d
manulon <mlongoe@fi.uba.ar>
2021-07-02 09:59:30 -0300

    Casi listo el editor_config.yaml

--- mapEditor/ButtonBox.cpp
+++ mapEditor/ButtonBox.cpp
@@ -18,11 +18,24 @@ bool ButtonBox::clicked(std::vector<Tile*>& options,const Image& image){
         options.pop_back();
     }
 
-    options.push_back(new Tile(21,0,500,image));
+    /*--- PARA AZTEC ---*/
+    /*options.push_back(new Tile(21,0,500,image));
     options.push_back(new Tile(22,48,500,image));
     options.push_back(new Tile(23,98,500,image));
     options.push_back(new Tile(26,0,548,image));
-    options.push_back(new Tile(27,48,548,image));
+    options.push_back(new Tile(27,48,548,image));*/
+
+    /*--- PARA DUST ---*/
+    options.push_back(new Tile(21,0,500,image));
+    options.push_back(new Tile(22,0,548,image));
+    options.push_back(new Tile(23,48,500,image));
+    options.push_back(new Tile(24,48,548,image));
+    options.push_back(new Tile(25,98,500,image));   
+    options.push_back(new Tile(26,98,548,image));
+    options.push_back(new Tile(27,146,500,image));
+    options.push_back(new Tile(28,146,548,image));
+    options.push_back(new Tile(29,194,548,image));
+    options.push_back(new Tile(30,194,500,image));
     
     return false;
 }
